dced1c502e343fbe7c0f04751441a1182b040f3c
15010416 <43272517+15010416@users.noreply.github.com>
2018-11-01 22:19:11 +0000

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,71 @@
+package Pontoon;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args){
+		Scanner kboard = new Scanner(System.in);
+        Deck pontoonDeck = new Deck();
+	    pontoonDeck.shuffle();
+
+	    String choice = "";
+
+	    Punter player1 = new Punter("Karen");
+	    Dealer house = new Dealer();
+
+	    player1.twist(player1.getPlayHand(),pontoonDeck.deal());
+        player1.twist(player1.getPlayHand(),pontoonDeck.deal());
+        System.out.println("Total hand = " + player1.getPlayHand().getTotalValue());
+        System.out.println("Total chips = " + player1.getNbrChips());
+        System.out.println("Do you wish to Buy (B), Twist (T) or Stick (S)?");
+        choice = kboard.nextLine();
+
+        while(choice.equalsIgnoreCase("b") || choice.equalsIgnoreCase("t") )
+        {
+            switch (choice.toUpperCase()){
+            case "B":
+                player1.buy(player1.getPlayHand(),pontoonDeck.deal());
+                break;
+            case "T":
+                player1.twist(player1.getPlayHand(),pontoonDeck.deal());
+                break;
+            case "S":
+                break;
+            default:
+                break;
+            }
+            System.out.println("Total hand = " + player1.getPlayHand().getTotalValue());
+            System.out.println("Total chips = " + player1.getNbrChips());
+            System.out.println("Do you wish to Buy (B), Twist (T) or Stick (S)?");
+            choice = kboard.nextLine();
+        }
+        kboard.close();
+
+        house.twist(house.getPlayHand(),pontoonDeck.deal());
+        house.twist(house.getPlayHand(),pontoonDeck.deal());
+        while(house.getPlayHand().getTotalValue() < 16){
+            house.twist(house.getPlayHand(),pontoonDeck.deal());
+        }
+        System.out.println("Dealer has " + house.getPlayHand().getTotalValue());
+	}
+	
+	public void PlayAgain() {
+		
+	 boolean gameOver;
+	 
+	if(gameOver==true) {
+		 
+		 System.out.println("Do you want to play again?")
+		 
+		 If(gameOver == true); {
+			 Object y;
+			if(answer==y); {
+				 this.pontoonplay(); }
+			 else { 
+				 this.gameOver();
+	    }
+	  }
+	}
+
+
